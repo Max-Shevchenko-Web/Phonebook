7b2a14bfe17fc7e3ad9827e6983c315783ed5f58
Шевченко Максим <shevchenko@gogpb.ru>
2020-11-11 17:23:04 +0200

    add Mongo DB

--- src/App.js
+++ src/App.js
@@ -1,34 +1,110 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import personsService from'./services/personsService';
+import Notification from './components/Notification ';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ search, setSearch ] = useState('')
+  const [ persons, setPersons ] = useState([]);
+  const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
+  const [ search, setSearch ] = useState('');
+  const [notification, setNotification] = useState({
+    message: null,
+    typeOfClass: ''
+  })
 
-  const addNote =(event) => {
+  useEffect(()=>{
+    personsService
+      .getAll()
+      .then(persons => {
+        setPersons(persons);
+      })
+  },[])
+
+  const dispatchNotification = (newNotification) => {
+    setNotification(newNotification)
+    setTimeout(() => {
+      setNotification({message: null, typeOfClass: ''})
+    }, 5000)
+  }
+
+  const addPerson =(event) => {
     event.preventDefault();
-
-    const isContains = persons.some(item => item.name === newName);;
-    console.log(isContains);
-    if (isContains) {
-      alert(`${newName} is already added to phonebook`)
-      return
-    }
 
     const newObject = {
       name: newName,
       number: newNumber
     }
 
-    setPersons(persons.concat(newObject));
-    setNewName('');
-    setNewNumber('');
+    // если такое имя уже есть всписке
+    const isContains = persons.some(item => {
+      if (item.name === newName) {
+        // предлагаем заменить ранее записаную запись
+        const result = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`);
+
+        if (result) {
+          personsService
+              .update(item.id, newObject)
+              .then(data => {
+                // обновляем список только в нужном нам месте
+                setPersons(persons.map(person => person.id !== item.id ? person : data))
+                dispatchNotification({
+                    message : `${newName} updated`,
+                    typeOfClass : 'info'
+                })
+              })
+        }
+        return true
+      } else {
+        return false
+      }
+    });
+
+    // если такое имени нет создаем новое
+    if(!isContains) {
+      personsService.create(newObject).then(person=> {
+        setPersons(persons.concat(person));
+        setNewName('');
+        setNewNumber('');
+        dispatchNotification({
+          message : `Added ${person.name}`,
+          typeOfClass : 'info'
+        })
+      })
+      .catch(error => {
+        console.log(error.response.data)
+        dispatchNotification({
+          // message: JSON.stringify(error.response.data),
+          message: `${newName} not added`,
+          typeOfClass: 'error'
+        })
+      })
+    }
+}
+
+  const deleteNote = ({name, id}) => {
+    const result = window.confirm(`Delete ${name} ?`);
+    if (result) {
+      personsService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+          dispatchNotification({
+            message : `${name} was removed`,
+            typeOfClass : 'info'
+          })
+        })
+        .catch(error => {
+          console.log(error);
+          dispatchNotification({
+            message: `Information of ${name} has already been removed from server`,
+            typeOfClass: 'error'
+          })
+          setPersons(persons.filter(p => p.id !== id))
+        })
+
+    } else {
+      return null
+    }
   }
 
   const toCompare = (str1, str2) => {
@@ -43,6 +119,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification.message} className={notification.typeOfClass}/>
       <div>
         filter shown with:&nbsp;
         <input
@@ -51,7 +128,7 @@ const App = () => {
         />
       </div>
       <h2>Add a new numbers</h2>
-      <form onSubmit={addNote}>
+      <form>
         <div>
           name:&nbsp;
           <input
@@ -67,13 +144,18 @@ const App = () => {
           />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {numbersToShow.map(person => {
-          return <li key={person.name} >{person.name}:{person.number}</li>
+          return (
+            <div key={person.name}>
+              <li  >{person.name}:{person.number}</li>
+              <button onClick={() => deleteNote(person)}>delete</button>
+            </div>
+          )
         })}
       </ul>
     </div>
